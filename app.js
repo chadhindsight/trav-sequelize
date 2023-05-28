7c59f0e7d0899371d3f5badfbd1524994f08fd01
chad hinds <chadrickj8@gmail.com>
2023-05-28 14:40:31 -0400

    Initial setup and package installation

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const express = require('express');
+const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('GRASS')
+})
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
